511f29f122b73343737952741b59357441204832
aliyul <waliprogrammer@gmail.com>
2025-03-29 07:01:37 +0700

    Update jasa-konstruksi-perbaikan-post.js

--- jasa-konstruksi-perbaikan-post.js
+++ jasa-konstruksi-perbaikan-post.js
@@ -51,8 +51,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const cleanUrlJasaPerbaikanKons = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
 
      // Menemukan elemen menggunakan Id
-     var materialKons = document.getElementById("materialKons");
-     var JasaKons = document.getElementById("JasaKons");
+    // var materialKons = document.getElementById("materialKons");
+    // var JasaKons = document.getElementById("JasaKons");
  
      var JasaKonstruksiLink = document.getElementById("JasaKonstruksi");
      var JasaRenovasiPerbaikanLink = document.getElementById("JasaRenovasiPerbaikan");
