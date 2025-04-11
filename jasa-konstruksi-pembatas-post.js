7d34301e9f5f2e88c6889d4a85ac80e7a9863543
aliyul <waliprogrammer@gmail.com>
2025-04-11 20:08:24 +0700

    Create jasa-konstruksi-pembatas-post.js

---
+++ jasa-konstruksi-pembatas-post.js
@@ -0,0 +1,291 @@
+
+const urlMappingPengamananAreaProyek = {
+"https://www.betonjayareadymix.com/p/jasa-pemasangan-safety-net-proyek.html": "Jasa Pemasangan Safety Net Proyek",
+  "https://www.betonjayareadymix.com/p/jasa-pemasangan-scaffolding-guard.html": "Jasa Pemasangan Scaffolding Guard",
+  "https://www.betonjayareadymix.com/p/jasa-pagar-sementara-proyek.html": "Jasa Pagar Sementara Proyek",
+  "https://www.betonjayareadymix.com/p/jasa-sistem-keamanan-perimeter-proyek.html": "Jasa Sistem Keamanan Perimeter Proyek"
+
+};
+
+const urlMappingRambuSistemKeamananVisual = {
+"https://www.betonjayareadymix.com/p/jasa-pemasangan-rambu-lalu-lintas.html": "Jasa Pemasangan Rambu Lalu Lintas",
+  "https://www.betonjayareadymix.com/p/jasa-pemasangan-cermin-tikungan.html": "Jasa Pemasangan Cermin Tikungan",
+  "https://www.betonjayareadymix.com/p/jasa-pemasangan-cat-marka-jalan.html": "Jasa Pemasangan Cat Marka Jalan",
+  "https://www.betonjayareadymix.com/p/jasa-pemasangan-warning-light.html": "Jasa Pemasangan Warning Light"
+
+};
+
+const urlMappingPengamananSisiJalan = {
+"https://www.betonjayareadymix.com/p/jasa-pemasangan-guardrail-besi.html": "Jasa Pemasangan Guardrail Besi",
+  "https://www.betonjayareadymix.com/p/jasa-pemasangan-railing-jalan.html": "Jasa Pemasangan Railing Jalan",
+  "https://www.betonjayareadymix.com/p/jasa-bollard-tiang-pengaman-jalan.html": "Jasa Bollard & Tiang Pengaman Jalan",
+  "https://www.betonjayareadymix.com/p/jasa-pagar-pembatas-flyover-jembatan.html": "Jasa Pagar Pembatas Flyover & Jembatan"
+
+};
+
+const urlMappingPembuatanPagarDinding = {
+"https://www.betonjayareadymix.com/p/jasa-pagar-beton-precast.html": "Jasa Pagar Beton Precast",
+  "https://www.betonjayareadymix.com/p/jasa-pagar-panel-beton.html": "Jasa Pagar Panel Beton",
+  "https://www.betonjayareadymix.com/p/jasa-pagar-besi-hollow-wiremesh.html": "Jasa Pagar Besi Hollow & Wiremesh",
+  "https://www.betonjayareadymix.com/p/jasa-dinding-pembatas-bata-batako.html": "Jasa Dinding Pembatas Bata & Batako",
+  "https://www.betonjayareadymix.com/p/jasa-pagar-kawat-harmonika.html": "Jasa Pagar Kawat Harmonika",
+  "https://www.betonjayareadymix.com/p/jasa-pagar-brc.html": "Jasa Pagar BRC",
+  "https://www.betonjayareadymix.com/p/jasa-pagar-rumah.html": "Jasa Pagar Rumah",
+  "https://www.betonjayareadymix.com/p/jasa-pagar-bangunan.html": "Jasa Pagar Bangunan"
+
+
+};
+
+//SUB urlMappingPembuatanPagarDinding
+const urlMappingPagarBetonPrecast = {
+
+};
+const urlMappingPagarPanelBeton = {
+ "https://www.betonjayareadymix.com/2018/11/harga-jasa-pasang-pagar-panel-beton.html": "Harga Jasa Pasang Pagar Panel Beton",
+  "https://www.betonjayareadymix.com/2018/11/pagar-panel-beton-terpasang.html": "Pagar Panel Beton Terpasang",
+  "https://www.betonjayareadymix.com/2018/11/harga-pasang-pagar-panel-beton.html": "Harga Pasang Pagar Panel Beton",
+  "https://www.betonjayareadymix.com/2018/11/biaya-pasang-pagar-panel-beton.html": "Biaya Pasang Pagar Panel Beton",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-jakarta.html": "Jasa Pasang Pagar Panel Beton Jakarta",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-bogor.html": "Jasa Pasang Pagar Panel Beton Bogor",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-bekasi.html": "Jasa Pasang Pagar Panel Beton Bekasi",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-depok.html": "Jasa Pasang Pagar Panel Beton Depok",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-tangerang.html": "Jasa Pasang Pagar Panel Beton Tangerang",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-karawang.html": "Jasa Pasang Pagar Panel Beton Karawang",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-cianjur.html": "Jasa Pasang Pagar Panel Beton Cianjur",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-cirebon.html": "Jasa Pasang Pagar Panel Beton Cirebon",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-ciamis.html": "Jasa Pasang Pagar Panel Beton Ciamis",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-kuningan.html": "Jasa Pasang Pagar Panel Beton Kuningan",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-bandung.html": "Jasa Pasang Pagar Panel Beton Bandung",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-indramayu.html": "Jasa Pasang Pagar Panel Beton Indramayu",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-majalengka.html": "Jasa Pasang Pagar Panel Beton Majalengka",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-tasikmalaya.html": "Jasa Pasang Pagar Panel Beton Tasikmalaya",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-purwakarta.html": "Jasa Pasang Pagar Panel Beton Purwakarta",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-perumahan.html": "Jasa Pasang Pagar Panel Beton Perumahan",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-pabrik.html": "Jasa Pasang Pagar Panel Beton Pabrik",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-gudang.html": "Jasa Pasang Pagar Panel Beton Gudang",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-lahan-kosong.html": "Jasa Pasang Pagar Panel Beton Lahan Kosong",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-kandang-ternak.html": "Jasa Pasang Pagar Panel Beton Kandang Ternak",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-pertambangan.html": "Jasa Pasang Pagar Panel Beton Pertambangan",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-sekolah.html": "Jasa Pasang Pagar Panel Beton Sekolah",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-rumah-sakit.html": "Jasa Pasang Pagar Panel Beton Rumah Sakit",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-tempat-ibadah.html": "Jasa Pasang Pagar Panel Beton Tempat Ibadah",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-pembatas-lahan.html": "Jasa Pasang Pagar Panel Beton Pembatas Lahan",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-keamanan.html": "Jasa Pasang Pagar Panel Beton Keamanan",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-kedap-suara.html": "Jasa Pasang Pagar Panel Beton Kedap Suara",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-tahan-lama.html": "Jasa Pasang Pagar Panel Beton Tahan Lama",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-cepat-dipasang.html": "Jasa Pasang Pagar Panel Beton Cepat Dipasang",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-tahan-banjir.html": "Jasa Pasang Pagar Panel Beton Tahan Banjir",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-motif.html": "Jasa Pasang Pagar Panel Beton Motif",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-polosan.html": "Jasa Pasang Pagar Panel Beton Polosan",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-custom.html": "Jasa Pasang Pagar Panel Beton Custom",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-tinggi.html": "Jasa Pasang Pagar Panel Beton Tinggi",
+  "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-panel-beton-rendah.html": "Jasa Pasang Pagar Panel Beton Rendah"
+  
+};
+const urlMappingPagarBesi = {
+
+};
+const urlMappingDindingBata = {
+
+};
+const urlMappingPagarKawat = {
+
+};
+const urlMappingPagarBRC = {
+
+};
+const urlMappingPagarRumah = {
+"https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-rumah-terdekat.html": "Jasa Pasang Pagar Rumah Terdekat",
+    "https://www.betonjayareadymix.com/2018/11/harga-jasa-pasang-pagar-rumah.html": "Harga Jasa Pasang Pagar Rumah",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-rumah-besi-beton-kayu.html": "Jasa Pasang Pagar Rumah Besi Beton Kayu",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-rumah-minimalis-mewah.html": "Jasa Pasang Pagar Rumah Minimalis Mewah",
+    "https://www.betonjayareadymix.com/2018/11/biaya-jasa-pasang-pagar-rumah.html": "Biaya Jasa Pasang Pagar Rumah",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-rumah-profesional.html": "Jasa Pasang Pagar Rumah Profesional",
+    "https://www.betonjayareadymix.com/2018/11/keunggulan-jasa-pasang-pagar-rumah.html": "Keunggulan Jasa Pasang Pagar Rumah",
+    "https://www.betonjayareadymix.com/2018/11/jasa-pasang-pagar-rumah-pondasi-kuat.html": "Jasa Pasang Pagar Rumah Pondasi Kuat",
+    "https://www.betonjayareadymix.com/2018/11/tukang-pasang-pagar-rumah.html": "Tukang Pasang Pagar Rumah"
+ 
+};
+const urlMappingPagarBangunan = {
+
+};
+// Menyimpan elemen yang dihapus dalam variabel
+let removedElementsJasaPembatasKonsPost = {};
+// Fungsi untuk menghapus elemen berdasarkan ID
+function removeCondition(conditionId) {
+    const conditionElement = document.getElementById(conditionId);
+
+    if (conditionElement) {
+        // Menyimpan elemen yang dihapus dalam objek untuk bisa dikembalikan
+        removedElementsJasaPembatasKonsPost[conditionId] = conditionElement;
+        conditionElement.remove(); // Menghapus elemen tersebut
+    }
+}
+
+// Fungsi untuk mengembalikan elemen yang telah dihapus
+function restoreCondition(conditionId) {
+    const breadcrumb = document.querySelector('.breadcrumb');
+    const elementToRestore = removedElementsJasaPembatasKonsPost[conditionId]; // Mendapatkan elemen yang disimpan
+
+    if (elementToRestore) {
+        breadcrumb.appendChild(elementToRestore); // Menambahkan elemen kembali ke dalam breadcrumb
+        delete removedElementsJasaPembatasKonsPost[conditionId]; // Menghapus elemen dari objek setelah dikembalikan
+    } else {
+        console.log(`Elemen dengan ID ${conditionId} tidak ditemukan di removedElementsJasaPembatasKonsPost.`);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    // var currentUrl = window.location.href;
+     //const cleanUrl = currentUrl.split('?')[0]; // Menghapus parameter seperti ?m=1
+    const cleanUrlJasaPembatasKonsPost = window.location.href.split(/[?#]/)[0]; // Menghilangkan parameter seperti ?m=1
+
+     // Menemukan elemen menggunakan Id
+    var JasaKonsPembatasPost = document.getElementById("JasaKonsPembatasPost");
+
+    if (!JasaKonsPembatasPost) {
+        console.error("elemen Id JasaKonsPembatasPost kondisi terhapus");
+        return;
+    }
+  
+	
+     var JasaKonstruksiPembatasLink = document.getElementById("JasaKonstruksiPembatasPost");
+     var JasaPembatasPostLink = document.getElementById("JasaPembatasPost");
+
+     //SUB JASA PEMBATAS
+     var JasaPembuatanPagarDindingPembatasLink = document.getElementById("JasaPembuatanPagarDindingPembatasPost");
+     var JasaPengamanSisiJalanInfrastrukturLink = document.getElementById("JasaPengamanSisiJalanInfrastrukturPost");
+     var JasaRambudanSistemKeamananVisualLink = document.getElementById("JasaRambudanSistemKeamananVisualPost");
+     var JasaPengamananAreaProyekLink = document.getElementById("JasaPengamananAreaProyekPost");
+
+    //SUB JasaPembuatanPagarDindingPembatas
+     var JasaPagarBetonPrecastLink = document.getElementById("JasaPagarBetonPrecastPost");
+     var JasaPagarPanelBetonLink = document.getElementById("JasaPagarPanelBetonPost");
+     var JasaPagarBesiHollowWiremeshLink = document.getElementById("JasaPagarBesiHollowWiremeshPost");
+     var JasaDindingPembatasBataBatakoLink = document.getElementById("JasaDindingPembatasBataBatakoPost");
+     var JasaPagarKawatHarmonikaLink = document.getElementById("JasaPagarKawatHarmonikaPost");
+     var JasaPagarBrcLink = document.getElementById("JasaPagarBrcPost");
+     var JasaPagarRumahLink = document.getElementById("JasaPagarRumahPost");
+     var JasaPagarBangunanLink = document.getElementById("JasaPagarBangunanPost");
+	
+     var pageNameJasaPembatasPost = document.getElementById("pageNameJasaPembatasPost");
+    
+
+     // Default untuk menyembunyikan elemen
+     JasaKonstruksiPembatasPostLink.style.visibility = 'hidden';
+     JasaPembatasPostLink.style.visibility = 'hidden';
+	
+     //SUB JASA PEMBATAS
+     JasaPembuatanPagarDindingPembatasPostLink.style.visibility = 'hidden';
+     JasaPengamanSisiJalanInfrastrukturPostLink.style.visibility = 'hidden';
+     JasaRambudanSistemKeamananVisualPostLink.style.visibility = 'hidden';
+     JasaPengamananAreaProyekPostLink.style.visibility = 'hidden';
+
+    //SUB JasaPembuatanPagarDindingPembatas
+     JasaPagarBetonPrecastPostLink.style.visibility = 'hidden';
+     JasaPagarPanelBetonPostLink.style.visibility = 'hidden';
+     JasaPagarBesiHollowWiremeshPostLink.style.visibility = 'hidden';
+     JasaDindingPembatasBataBatakoPostLink.style.visibility = 'hidden';
+     JasaPagarKawatHarmonikaPostLink.style.visibility = 'hidden';
+     JasaPagarBrcPostLink.style.visibility = 'hidden';
+     JasaPagarRumahPostLink.style.visibility = 'hidden';
+     JasaPagarBangunanPostLink.style.visibility = 'hidden';
+	
+     pageNameJasaPembatasPost.textContent = "";
+	
+    //SUB urlMappingPembuatanPagarDinding
+
+       if (urlMappingPagarPanelBeton[cleanUrlJasaPembatasKons]) {
+        restoreCondition('JasaKonsPembatasPost');
+        restoreCondition('JasaPembuatanPagarDindingPembatasPost');
+        restoreCondition('JasaPagarPanelBetonPost');
+	       
+     
+        //hapus elemen div id lain
+        removeCondition('materialKonsReadymix');
+        removeCondition('ProdukKonsSaluran');
+        removeCondition('ProdukKonsPembatas');
+        removeCondition('JasaKonsPerbaikan');
+       	removeCondition('JasaKons');
+       	removeCondition('JasaKonsSub');
+       	removeCondition('MenuKons');
+       	removeCondition('JasaKonsFinishing');
+        removeCondition('JasaKonsStruktur');
+
+
+        //hapus elemen SUB jasa pembatas lain
+        removeCondition('JasaPengamanSisiJalanInfrastrukturPost');
+        removeCondition('JasaRambudanSistemKeamananVisualPost');
+        removeCondition('JasaPengamananAreaProyekPost');
+
+	 //hapus elemen sub JasaPembuatanPagarDindingPembatas SELAIN JasaPagarPanelBeton
+        removeCondition('JasaPagarBetonPrecastPost');
+       // removeCondition('JasaPagarPanelBetonPost');
+        removeCondition('JasaPagarBesiHollowWiremeshPost'); 
+	removeCondition('JasaDindingPembatasBataBatakoPost');
+	removeCondition('JasaPagarKawatHarmonikaPost');
+	removeCondition('JasaPagarBrcPost');
+	removeCondition('JasaPagarRumahPost');
+	removeCondition('JasaPagarBangunanPost');
+       
+        JasaKonstruksiPembatasPostLink.style.visibility = 'visible';
+        JasaPembatasPostLink.style.visibility = 'visible';
+        JasaPembuatanPagarDindingPembatasPostLink.style.visibility = 'visible';
+	//TAMPILKAN JasaPagarPanelBeton
+        JasaPagarPanelBetonPostLink.style.visibility = 'visible';
+        pageNameJasaPembatasPost.textContent = urlMappingPagarPanelBeton[cleanUrlJasaPembatasKonsPost];
+    }
+    
+	
+    if (urlMappingPagarBetonPrecast[cleanUrlJasaPembatasKons]) {
+        restoreCondition('JasaKonsPembatas');
+        restoreCondition('JasaPasangPagarBeton');
+
+       //hapus elemen div id lain
+        removeCondition('materialKonsReadymix');
+        removeCondition('ProdukKonsSaluran');
+        removeCondition('ProdukKonsPembatas');
+        removeCondition('JasaKonsPerbaikan');
+       	removeCondition('JasaKons');
+       	removeCondition('JasaKonsSub');
+       	removeCondition('MenuKons');
+       	removeCondition('JasaKonsFinishing');
+        removeCondition('JasaKonsStruktur');
+	
+        //hapus elemen jasa pembatas lain
+        removeCondition('JasaPasangPagarPanelBeton');
+        removeCondition('JasaPasangPagarRumah');
+        
+        JasaKonstruksiPembatasLink.style.visibility = 'visible';
+        JasaPembatasLink.style.visibility = 'visible';
+        JasaPasangPagarBetonLink.style.visibility = 'visible';
+        pageNameJasaPembatas.textContent = urlMappingPagarBetonPrecast[cleanUrlJasaPembatasKons];
+    }
+
+   if (urlMappingPagarRumah[cleanUrlJasaPembatasKons]) {
+        restoreCondition('JasaKonsPembatas');
+        restoreCondition('JasaPasangPagarRumah');
+
+       //hapus elemen div id lain
+        removeCondition('materialKonsReadymix');
+        removeCondition('ProdukKonsSaluran');
+        removeCondition('ProdukKonsPembatas');
+        removeCondition('JasaKonsPerbaikan');
+       	removeCondition('JasaKons');
+       	removeCondition('JasaKonsSub');
+       	removeCondition('MenuKons');
+       	removeCondition('JasaKonsFinishing');
+        removeCondition('JasaKonsStruktur');
+	
+        //hapus elemen jasa pembatas lain
+        removeCondition('JasaPasangPagarPanelBeton');
+        removeCondition('JasaPasangPagarBeton');
+    
+        JasaKonstruksiPembatasLink.style.visibility = 'visible';
+        JasaPembatasLink.style.visibility = 'visible';
+        JasaPasangPagarRumahLink.style.visibility = 'visible';
+        pageNameJasaPembatas.textContent = urlMappingPagarRumah[cleanUrlJasaPembatasKons];
+    }
+
+   });
+
