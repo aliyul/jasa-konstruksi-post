47041f1c4e5b77f6e0ac21a15469c10f699a1456
aliyul <waliprogrammer@gmail.com>
2025-04-11 19:17:51 +0700

    Update jasa-konstruksi-pembatas.js

--- jasa-konstruksi-pembatas.js
+++ jasa-konstruksi-pembatas.js
@@ -142,10 +142,13 @@ document.addEventListener("DOMContentLoaded", function() {
 
      // Menemukan elemen menggunakan Id
     var JasaKonsPembatas = document.getElementById("JasaKonsPembatas");
+	/*
     if (!JasaKonsPembatas) {
         console.error("elemen Id JasaKonsPembatas kondisi terhapus");
         return;
     }
+    */
+	
      var JasaKonstruksiPembatasLink = document.getElementById("JasaKonstruksiPembatas");
      var JasaPembatasLink = document.getElementById("JasaPembatas");
 
