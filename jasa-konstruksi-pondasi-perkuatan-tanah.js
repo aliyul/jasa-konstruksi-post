a9e511c033749e30bd7e8c03d59bee4d5e8ccbb5
aliyul <waliprogrammer@gmail.com>
2025-04-12 11:47:05 +0700

    Create jasa-konstruksi-pondasi-perkuatan-tanah.js

---
+++ jasa-konstruksi-pondasi-perkuatan-tanah.js
@@ -0,0 +1,6 @@
+"https://www.betonjayareadymix.com/p/jasa-pondasi-bangunan.html": "Jasa Pondasi Bangunan",
+//Jasa Pemadatan dan Persiapan Lahan ini terlalu UMUM ga FKS ke pondasi
+  //"https://www.betonjayareadymix.com/p/jasa-pemadatan-dan-persiapan-lahan.html": "Jasa Pemadatan dan Persiapan Lahan",
+"https://www.betonjayareadymix.com/p/jasa-pemadatan-persiapan-pondasi.html": "Jasa Pemadatan & Persiapan Pondasi",
+  "https://www.betonjayareadymix.com/p/jasa-perkuatan-tanah.html": "Jasa Perkuatan Tanah",
+  "https://www.betonjayareadymix.com/p/jasa-retrofitting-pondasi.html": "Jasa Retrofitting Pondasi"
