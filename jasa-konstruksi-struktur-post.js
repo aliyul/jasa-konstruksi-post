8866b7b54b733e6c3345ce787689af2da8bf2b17
aliyul <waliprogrammer@gmail.com>
2025-04-28 21:58:19 +0700

    Update jasa-konstruksi-struktur-post.js

--- jasa-konstruksi-struktur-post.js
+++ jasa-konstruksi-struktur-post.js
@@ -41,6 +41,8 @@ const urlMappingJasaStrukturBajaGudang = {
 */
 //SUB JasaStrukturBetondanPengecoranPost
 const urlMappingJasaPengecoranLantaiDak = {
+  "https://www.betonjayareadymix.com/2018/10/harga-jasa-dak-beton.html": "Harga Jasa Dak Beton",
+
   "https://www.betonjayareadymix.com/2018/09/borongan-ring-balok.html": "Borongan ring balok",
 
 "https://www.betonjayareadymix.com/2018/09/borongan-sloof.html": "Borongan Sloof",
