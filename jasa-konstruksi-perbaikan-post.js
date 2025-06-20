812b03a09da68f2d43a68a636f713c3239fee263
aliyul <waliprogrammer@gmail.com>
2025-06-20 17:34:44 +0700

    Update jasa-konstruksi-perbaikan-post.js

--- jasa-konstruksi-perbaikan-post.js
+++ jasa-konstruksi-perbaikan-post.js
@@ -230,8 +230,8 @@ const urlMappingJasaPerbaikanBetonRetak = {
 };
 const urlMappingJasaChippingBeton = {
   "https://www.betonjayareadymix.com/2019/06/harga-jasa-chipping-beton.html": "Harga Jasa Chipping Beton",
-  "https://www.betonjayareadymix.com/2019/06/harga-chipping-per-m3.html": "Harga Chipping Per M3",
-  "https://www.betonjayareadymix.com/2019/06/harga-chipping-per-m2.html": "Harga Chipping Per M2",
+  "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m3.html": "Harga Chipping Beton Per M3",
+  "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-murah.html": "Harga Chipping Beton Murah",
     "https://www.betonjayareadymix.com/2019/06/harga-chipping-beton-per-m2.html": "Harga Chipping Beton Per M2",
   "https://www.betonjayareadymix.com/2019/06/harga-pekerjaan-chipping-beton.html": "Harga Pekerjaan Chipping Beton"
 
