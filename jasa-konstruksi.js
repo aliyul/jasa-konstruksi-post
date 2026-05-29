f467f445af70ba153290c3b52bbb742725909199
aliyul <waliprogrammer@gmail.com>
2026-05-29 11:21:48 +0700

    Update jasa-konstruksi.js

--- jasa-konstruksi.js
+++ jasa-konstruksi.js
@@ -387,7 +387,8 @@ const urlMappingFinishingFromMoneyMasterMoneyMaster1 = {
 	  "https://www.betonjayareadymix.com/p/jasa-pasang-wpc.html": "Jasa Pasang WPC",
   "https://www.betonjayareadymix.com/p/jasa-pasang-vinyl.html": "Jasa Pasang Vinyl",
   "https://www.betonjayareadymix.com/p/jasa-pasang-pvc.html": "Jasa Pasang PVC", 
-  "https://www.betonjayareadymix.com/p/jasa-pasang-plafon.html": "Jasa Pasang Plafon",  
+  "https://www.betonjayareadymix.com/p/jasa-pasang-plafon.html": "Jasa Pasang Plafon",
+"https://www.betonjayareadymix.com/p/jasa-profil-beton.html": "Jasa Profil Beton",
 	
 	"https://www.betonjayareadymix.com/p/jasa-finishing-dinding.html": "Jasa Finishing Dinding", 
 	"https://www.betonjayareadymix.com/p/jasa-finishing-furniture.html": "Jasa Finishing Furniture", 
