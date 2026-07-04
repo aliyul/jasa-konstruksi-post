e43372ded027649ecc84edc064dc8e14348aad42
aliyul <waliprogrammer@gmail.com>
2026-07-04 21:18:20 +0700

    Update jasa-konstruksi-finishing-post.js

--- jasa-konstruksi-finishing-post.js
+++ jasa-konstruksi-finishing-post.js
@@ -3845,11 +3845,10 @@ if (urlMappingJasaPasangKeramikLantaiFromMoneyPageMoneyChild[cleanUrlJasaKonsFin
         urlMappingJasaPasangWpcFromMoneyMaster1MoneyChild,
         cleanUrlJasaKonsFinishingPost,
         [
-            //{ name: 'Beton Jaya Readymix', url: 'https://www.betonjayareadymix.com/' },
             { name: 'Jasa Konstruksi', url: 'https://www.betonjayareadymix.com/p/jasa-konstruksi.html' },
-            { name: 'Daftar Jasa Finishing', url: 'https://www.betonjayareadymix.com/p/daftar-jasa-finishing.html' },
-            { name: 'Perbandingan Jasa Finishing', url: 'https://www.betonjayareadymix.com/p/perbandingan-jasa-finishing.html'},
-            { name: 'Jasa Finishing', url: 'https://www.betonjayareadymix.com/p/jasa-finishing.html' },
+            { name: 'Daftar Jasa Pasang', url: 'https://www.betonjayareadymix.com/p/daftar-jasa-pasang.html'},
+            { name: 'Perbandingan Jasa Pasang', url: 'https://www.betonjayareadymix.com/p/perbandingan-jasa-pasang.html'},
+            { name: 'Jasa Pasang', url: 'https://www.betonjayareadymix.com/p/jasa-pasang.html'},
  			{ name: 'Jasa Pasang WPC', url: 'https://www.betonjayareadymix.com/p/jasa-pasang-wpc.html' }
            
         ],
