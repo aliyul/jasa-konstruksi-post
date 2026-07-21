2136147bced0ca9c4a941b62a7526c023362678c
aliyul <waliprogrammer@gmail.com>
2026-07-21 09:08:24 +0700

    Update jasa-bongkar-bangunan-post.js

--- jasa-bongkar-bangunan-post.js
+++ jasa-bongkar-bangunan-post.js
@@ -68,14 +68,23 @@ const urlMappingJasaBongkarRumahFromMoneyMaster1MoneyPage = {
    "https://www.betonjayareadymix.com/2019/08/jasa-bongkar-rumah-semi-permanen.html": "Jasa Bongkar Rumah Semi Permanen"
 };
 const urlMappingJasaBongkarRumahFromMoneyMaster1MoneyChild = {
-   "https://www.betonjayareadymix.com/p/jasa-bongkar-rumah-terdekat.html": "Jasa Bongkar Rumah Terdekat",
-   "https://www.betonjayareadymix.com/p/jasa-bongkar-rumah-jakarta.html": "Jasa Bongkar Rumah Jakarta",
-   "https://www.betonjayareadymix.com/p/jasa-bongkar-rumah-bogor.html": "Jasa Bongkar Rumah Bogor",
-   "https://www.betonjayareadymix.com/p/jasa-bongkar-rumah-depok.html": "Jasa Bongkar Rumah Depok",
-   "https://www.betonjayareadymix.com/p/jasa-bongkar-rumah-tangerang.html": "Jasa Bongkar Rumah Tangerang",
-   "https://www.betonjayareadymix.com/p/jasa-bongkar-rumah-bekasi.html": "Jasa Bongkar Rumah Bekasi",
-   "https://www.betonjayareadymix.com/p/jasa-bongkar-rumah-karawang.html": "Jasa Bongkar Rumah Karawang",
-   "https://www.betonjayareadymix.com/p/jasa-bongkar-rumah-surabaya.html": "Jasa Bongkar Rumah Surabaya"
+   "https://www.betonjayareadymix.com/2018/09/jasa-bongkar-rumah-terdekat.html": "Jasa Bongkar Rumah Terdekat",
+   "https://www.betonjayareadymix.com/2018/09/jasa-bongkar-rumah-jakarta.html": "Jasa Bongkar Rumah Jakarta",
+   "https://www.betonjayareadymix.com/2018/09/jasa-bongkar-rumah-bogor.html": "Jasa Bongkar Rumah Bogor",
+   "https://www.betonjayareadymix.com/2018/09/jasa-bongkar-rumah-depok.html": "Jasa Bongkar Rumah Depok",
+   "https://www.betonjayareadymix.com/2018/09/jasa-bongkar-rumah-tangerang.html": "Jasa Bongkar Rumah Tangerang",
+   "https://www.betonjayareadymix.com/2018/09/jasa-bongkar-rumah-bekasi.html": "Jasa Bongkar Rumah Bekasi",
+   "https://www.betonjayareadymix.com/2018/09/jasa-bongkar-rumah-karawang.html": "Jasa Bongkar Rumah Karawang",
+   "https://www.betonjayareadymix.com/2018/09/jasa-bongkar-rumah-surabaya.html": "Jasa Bongkar Rumah Surabaya"
+};
+const urlMappingHargaJasaBongkarRumahFromMoneyPageMoneyChild = {
+   "https://www.betonjayareadymix.com/2018/09/harga-jasa-bongkar-rumah-jakarta.html": "Harga Jasa Bongkar Rumah Jakarta",
+   "https://www.betonjayareadymix.com/2018/09/harga-jasa-bongkar-rumah-bogor.html": "Harga Jasa Bongkar Rumah Bogor",
+   "https://www.betonjayareadymix.com/2018/09/harga-jasa-bongkar-rumah-depok.html": "Harga Jasa Bongkar Rumah Depok",
+   "https://www.betonjayareadymix.com/2018/09/harga-jasa-bongkar-rumah-tangerang.html": "Harga Jasa Bongkar Rumah Tangerang",
+   "https://www.betonjayareadymix.com/2018/09/harga-jasa-bongkar-rumah-bekasi.html": "Harga Jasa Bongkar Rumah Bekasi",
+   "https://www.betonjayareadymix.com/2018/09/harga-jasa-bongkar-rumah-karawang.html": "Harga Jasa Bongkar Rumah Karawang",
+   "https://www.betonjayareadymix.com/2018/09/harga-jasa-bongkar-rumah-surabaya.html": "Harga Jasa Bongkar Rumah Surabaya"
 };
 // ============================================================
 // 📁 JASA BONGKAR BETON - MONEY PAGE (BUKAN MONEY MASTER)
@@ -2164,6 +2173,7 @@ document.addEventListener("DOMContentLoaded", function() {
 		urlMappingJasaBongkarTowerFromMoneyMaster1MoneyPage,
 		urlMappingJasaBongkarRumahFromMoneyMaster1MoneyPage,
         urlMappingJasaBongkarRumahFromMoneyMaster1MoneyChild,
+		urlMappingHargaJasaBongkarRumahFromMoneyPageMoneyChild,
 		
 		urlMappingJasaBongkarBetonFromMoneyMaster1MoneyPage,
 		urlMappingJasaBongkarBetonFromMoneyMaster1MoneyChild,
@@ -2797,6 +2807,23 @@ if (urlMappingJasaBongkarRumahFromMoneyMaster1MoneyChild[cleanUrlJasaKonsBongkar
             { name: 'Perbandingan Jasa Bongkar Bangunan', url: 'https://www.betonjayareadymix.com/p/perbandingan-jasa-bongkar-bangunan.html' },
              { name: 'Jasa Bongkar Bangunan', url: 'https://www.betonjayareadymix.com/p/jasa-bongkar-bangunan.html' },
             { name: 'Jasa Bongkar Rumah', url: 'https://www.betonjayareadymix.com/p/jasa-bongkar-rumah.html' }
+        ],
+        'JASA_KONSTRUKSI'
+    );
+	
+}
+if (urlMappingHargaJasaBongkarRumahFromMoneyPageMoneyChild[cleanUrlJasaKonsBongkarBangunanPost]) {
+       generateBreadcrumbJasaBongkarBangunanPost(
+        urlMappingHargaJasaBongkarRumahFromMoneyPageMoneyChild,
+        cleanUrlJasaKonsBongkarBangunanPost,
+        [
+            //{ name: 'Beton Jaya Readymix', url: 'https://www.betonjayareadymix.com/' },
+            { name: 'Jasa Konstruksi', url: 'https://www.betonjayareadymix.com/p/jasa-konstruksi.html' },
+             { name: 'Daftar Jasa Bongkar Bangunan', url: 'https://www.betonjayareadymix.com/p/daftar-jasa-bongkar-bangunan.html' },
+            { name: 'Perbandingan Jasa Bongkar Bangunan', url: 'https://www.betonjayareadymix.com/p/perbandingan-jasa-bongkar-bangunan.html' },
+             { name: 'Jasa Bongkar Bangunan', url: 'https://www.betonjayareadymix.com/p/jasa-bongkar-bangunan.html' },
+            { name: 'Jasa Bongkar Rumah', url: 'https://www.betonjayareadymix.com/p/jasa-bongkar-rumah.html' },
+            { name: 'Harga Jasa Bongkar Rumah', url: 'https://www.betonjayareadymix.com/2018/09/harga-jasa-bongkar-rumah.html' }
         ],
         'JASA_KONSTRUKSI'
     );
