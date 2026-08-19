c4cea95b543c5676ac056413cc64a8cb1fac2ab7
aliyul <waliprogrammer@gmail.com>
2026-08-19 09:36:22 +0700

    Update jasa-konstruksi-pembatas-post.js

--- jasa-konstruksi-pembatas-post.js
+++ jasa-konstruksi-pembatas-post.js
@@ -77,7 +77,7 @@ const urlMappingJasaPasangPagarPanelBetonFromMoneyPageMoneyPage1  = {
   // ============================================================
   "https://www.betonjayareadymix.com/2018/09/biaya-pasang-pagar-panel-beton.html": "Biaya Pasang Pagar Panel Beton",
   "https://www.betonjayareadymix.com/2018/09/harga-jasa-pasang-pagar-panel-beton.html": "Harga Jasa Pasang Pagar Panel Beton",
-  "https://www.betonjayareadymix.com/2018/09/pagar-panel-beton-terpasang.html": "Pagar Panel Beton Terpasang",
+  "https://www.betonjayareadymix.com/2018/09/jasa-pagar-panel-beton-terpasang.html": "Jasa Pagar Panel Beton Terpasang",
   "https://www.betonjayareadymix.com/2018/09/jasa-pasang-pagar-panel-beton-per-meter.html": "Jasa Pasang Pagar Panel Beton Per Meter",
 
   
