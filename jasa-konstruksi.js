1af9c99b3e9ec2508f2b8d4fe2b7a5cf8b98f7e3
aliyul <waliprogrammer@gmail.com>
2026-09-25 08:55:03 +0700

    Update jasa-konstruksi.js

--- jasa-konstruksi.js
+++ jasa-konstruksi.js
@@ -451,7 +451,8 @@ const urlMappingJasaPengeboranFromSub1MoneyMaster = {
   "https://www.betonjayareadymix.com/p/jasa-bor-sumur-murah.html": "Jasa Bor Sumur Murah", 
   "https://www.betonjayareadymix.com/p/jasa-bore-pile.html": "Jasa Bore Pile",
    "https://www.betonjayareadymix.com/p/jasa-strauss-pile.html": "Jasa Strauss Pile",
-   
+     "https://www.betonjayareadymix.com/p/jasa-borongan-strauss-pile.html": "Jasa Borongan Strauss Pile",  
+  "https://www.betonjayareadymix.com/p/jasa-borongan-bore-pile.html": "Jasa Borongan Bore Pile", 
   "https://www.betonjayareadymix.com/p/jasa-bore-pile-murah.html": "Jasa Bore Pile Murah",  
 
   "https://www.betonjayareadymix.com/p/jasa-pengeboran-bore-pile.html": "Jasa Pengeboran Bore Pile",
